0aa3af2c638d715d77a374c09622f1028a638ea4
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-13 18:57:51 +0000

    Updated create functionality to create new bookmark object at end of code

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,4 +1,5 @@
 require "bookmark"
+require_relative "./database_helpers"
 
 describe Bookmark do
 
@@ -24,12 +25,13 @@ describe Bookmark do
 
   describe '#create' do
     it 'adds a bookmark to database' do
-      
-      # bookmark = Bookmark.create('https://www.github.com', 'That code place').first
-      bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark').first
+      bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
+      persisted_data = persisted_data(id: bookmark.id)
 
-      expect(bookmark['url']).to eq 'http://www.testbookmark.com'
-      expect(bookmark['title']).to eq 'Test Bookmark'
+      expect(bookmark).to be_a Bookmark
+      expect(bookmark.id).to eq persisted_data['id']
+      expect(bookmark.title).to eq 'Test Bookmark'
+      expect(bookmark.url).to eq 'http://www.testbookmark.com'
     end
   end
 
